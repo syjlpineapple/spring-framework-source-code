ed27c4af1c2609f38e6b161b62d69cc8fc876103
lee <zhi.li@quarkintl.com>
2024-10-31 10:41:12 +0800

    源码测试

---
+++ spring-demo/src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.springdemo;
+
+import com.example.springdemo.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringDemoApplication.class, args);
+		MyService myService = context.getBean(MyService.class);
+		myService.say();
+    }
+}
